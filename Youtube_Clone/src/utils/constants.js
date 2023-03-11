a18dc1440d7762c04bdc1b5851f9ed3a8ac0734a
Rob.Black <76528934+robfeketer3@users.noreply.github.com>
2023-03-11 12:42:30 +0100

    Add files via upload

---
+++ Youtube_Clone/src/utils/constants.js
@@ -0,0 +1,35 @@
+import CodeIcon from '@mui/icons-material/Code';
+import CssIcon from '@mui/icons-material/Css';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import CodeOffIcon from '@mui/icons-material/CodeOff';
+import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import DataArrayIcon from '@mui/icons-material/DataArray';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import SourceIcon from '@mui/icons-material/Source';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Fireship', icon: <TerminalIcon />, },
+  { name: 'ReactJS', icon: <CodeOffIcon />, },
+  { name: 'NextJS', icon: <IntegrationInstructionsIcon />, },
+  { name: 'JS Mastery', icon: <JavascriptIcon /> },
+  { name: 'Web Dev Simplified', icon: <CssIcon />, },
+  { name: 'freeCodeCamp', icon: <DataArrayIcon />, },
+  { name: 'The Coding Train', icon: <DataObjectIcon />, },
+  { name: 'Codecourse', icon: <DeveloperModeIcon />, },
+  { name: 'LearnWebCode', icon: <SourceIcon />, },
+  { name: 'LearnCode.academy', icon: <VpnKeyIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
